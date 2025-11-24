3b5f2c136fd28671500cd0a91eb09b1b2f925872
Uoemai <218074189+uoemai@users.noreply.github.com>
2025-11-24 13:12:43 +0100

    chore: update testcontainers for newer Docker API

--- service/src/test/java/org/whispersystems/textsecuregcm/storage/KEMPreKeyPageTest.java
+++ service/src/test/java/org/whispersystems/textsecuregcm/storage/KEMPreKeyPageTest.java
@@ -4,8 +4,8 @@
  */
 package org.whispersystems.textsecuregcm.storage;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
