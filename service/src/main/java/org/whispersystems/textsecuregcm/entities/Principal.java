a71e75884236297e83405f4db863285a6b15c84d
Uoemai <218074189+uoemai@users.noreply.github.com>
2025-11-28 12:55:21 +0100

    chore: add Molly notice in most changed files

--- service/src/main/java/org/whispersystems/textsecuregcm/entities/Principal.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/entities/Principal.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025 Molly Instant Messenger
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 package org.whispersystems.textsecuregcm.entities;
 
 import org.whispersystems.textsecuregcm.util.InvalidPrincipalException;
